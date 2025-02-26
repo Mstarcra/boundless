6dfb1c56a8df973b3331a6f0d63b3ae7c39f80c9
Nue <152944912+Mstarcra@users.noreply.github.com>
2025-02-26 14:56:02 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,19 +1,18 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-dotenv.config();
+// dotenv.config();
 
+console.log( process.env.DB_HOST);
 const db = await mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  dialectOptions: {
-      ssl: {
-        ca: process.env.DB_SSL_CA, // 添加 CA 證書
-        rejectUnauthorized: true, // 強制驗證證書
-      },
-    },
+  ssl: {
+    ca: process.env.DB_SSL_CA, // 修正為直接 ssl 配置
+    rejectUnauthorized: true,
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 10,
